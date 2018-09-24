63d401f8e8038216dcbca3e8f0ffaf6f12b8168d
wing <741126367@qq.com>
2018-09-24 12:40:25 +0800

    demo

---
+++ demo.sh
@@ -0,0 +1,18 @@
+mkdir demo
+cd demo
+mkdir css js
+touch index.html css/style.css js/main.js
+echo " <!DOCTYPE>
+<html>
+<head>
+ <title>Hello</title>
+</head>
+<body>
+ <h1>Hi</h1>
+</body>
+</html>
+"  > index.html
+echo "h1{color: red;}" > css/style.css
+echo "var string = 'Hello World'
+ alert(string)" > js/main.js
+exit
